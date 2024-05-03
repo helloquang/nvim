28a441c4bb69cf05ee6189cf2d074b86d55b9d9e
Hello Quang <hello.quangnguyen@gmail.com>
2024-05-03 23:11:26 +0700

    Update keymaps.lua

--- lua/kin/core/keymaps.lua
+++ lua/kin/core/keymaps.lua
@@ -23,6 +23,14 @@ keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window" })
 keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window" })
 keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window" })
 
+-- diagnostic
+  keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+  keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Prev Diagnostic" })
+  keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next Diagnostic" })
+
+  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
