f5a565ee0eb895f22a50fcf2eb9a04b2d4ef0d57
Hello Quang <hello.quangnguyen@gmail.com>
2024-05-03 07:26:14 +0700

    Initial commit

---
+++ lua/kin/core/init.lua
@@ -0,0 +1,2 @@
+require("kin.core.options")
+require("kin.core.keymaps")
---
+++ lua/kin/core/keymaps.lua
@@ -0,0 +1,26 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+---------------------
+-- General Keymaps -------------------
+
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- delete single character without copying into register
+keymap.set("n", "x", '"_x')
+
+keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
