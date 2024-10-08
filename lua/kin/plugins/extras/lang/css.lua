7c9bdc4f235eee3cabe95c8baf9b1a2d0f794f78
hellomochi <hello.quangnguyen@gmail.com>
2024-10-08 22:27:10 +0700

    Update files

--- lua/kin/plugins/extras/lang/css.lua
+++ lua/kin/plugins/extras/lang/css.lua
@@ -3,30 +3,25 @@ return {
 		"neovim/nvim-lspconfig",
 		opts = {
 			servers = {
-				ts_ls = {
+				cssls = {
 					settings = {},
 				},
 			},
 		},
 	},
-
 	{
 		"stevearc/conform.nvim",
 		opts = {
 			formatters_by_ft = {
-				javascript = { { "prettierd", "prettier" } },
-				typescript = { { "prettierd", "prettier" } },
-				typescriptreact = { { "prettierd", "prettier" } },
+				css = { { "prettierd", "prettier" } },
 			},
 		},
 	},
-
 	{
 		"nvim-treesitter/nvim-treesitter",
 		opts = {
 			ensure_installed = {
-				"javascript",
-				"typescript",
+				"css",
 			},
 		},
 	},
