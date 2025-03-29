bea7c94ad45d633efb071e8376197772e147645a
Quang Nguyễn <hello.quangnguyen@gmail.com>
2025-03-29 10:32:07 +0700

    Remove dashboard lua

--- lua/kin/plugins/lsp-config.lua
+++ lua/kin/plugins/lsp-config.lua
@@ -1,7 +1,7 @@
 return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
-		{ "williamboman/mason.nvim", config = true },
+		{ "williamboman/mason.nvim" },
 		{ "williamboman/mason-lspconfig.nvim" },
 		{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
 		{ "saghen/blink.cmp" },
