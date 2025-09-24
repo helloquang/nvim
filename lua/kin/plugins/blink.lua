8582b8cc1f92edf4f45ef708245c458d87448359
Quang Nguyen <quang.nguyen@vietunion.com.vn>
2025-09-24 13:27:31 +0700

    Update blink.lua

--- lua/kin/plugins/blink.lua
+++ lua/kin/plugins/blink.lua
@@ -5,49 +5,49 @@ return {
 		version = "*",
 		dependencies = {
 			"L3MON4D3/LuaSnip",
-			"Kaiser-Yang/blink-cmp-dictionary",
+			-- "Kaiser-Yang/blink-cmp-dictionary",
 			"nvim-lua/plenary.nvim",
 		},
 		keys = {
-			{
-				"<C-d>",
-				function()
-					require("blink.cmp").show({ providers = { "dictionary" } })
-				end,
-				mode = "i",
-				desc = "Dictionary",
-			},
+			-- {
+			-- 	"<C-d>",
+			-- 	function()
+			-- 		require("blink.cmp").show({ providers = { "dictionary" } })
+			-- 	end,
+			-- 	mode = "i",
+			-- 	desc = "Dictionary",
+			-- },
 		},
 		opts = {
 			snippets = { preset = "luasnip" },
 			sources = {
 				default = { "lsp", "path", "buffer", "snippets" },
-				providers = {
-					dictionary = {
-						module = "blink-cmp-dictionary",
-						max_items = 5,
-						min_keyword_length = 5,
-						opts = {
-							-- dictionary_files = { vim.fn.stdpath("config") .. "/data/dictionary/words_alpha.txt" },
-							dictionary_files = nil,
-							dictionary_directories = nil,
-							get_command = "rg",
-							get_command_args = function(prefix, _)
-								local dictionary_file1 = vim.fn.stdpath("config") .. "/data/dictionary/words_alpha.txt"
-								return {
-									"--color=never",
-									"--no-line-number",
-									"--no-messages",
-									"--no-filename",
-									"--smart-case",
-									"--",
-									prefix,
-									dictionary_file1,
-								}
-							end,
-						},
-					},
-				},
+				-- providers = {
+				-- 	dictionary = {
+				-- 		module = "blink-cmp-dictionary",
+				-- 		max_items = 5,
+				-- 		min_keyword_length = 5,
+				-- 		opts = {
+				-- 			-- dictionary_files = { vim.fn.stdpath("config") .. "/data/dictionary/words_alpha.txt" },
+				-- 			dictionary_files = nil,
+				-- 			dictionary_directories = nil,
+				-- 			get_command = "rg",
+				-- 			get_command_args = function(prefix, _)
+				-- 				local dictionary_file1 = vim.fn.stdpath("config") .. "/data/dictionary/words_alpha.txt"
+				-- 				return {
+				-- 					"--color=never",
+				-- 					"--no-line-number",
+				-- 					"--no-messages",
+				-- 					"--no-filename",
+				-- 					"--ignore-case",
+				-- 					"--",
+				-- 					prefix,
+				-- 					dictionary_file1,
+				-- 				}
+				-- 			end,
+				-- 		},
+				-- 	},
+				-- },
 			},
 			completion = {
 				menu = {
